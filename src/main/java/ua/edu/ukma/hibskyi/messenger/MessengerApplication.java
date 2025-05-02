698c604a5ca14d8b2b11c3c216cd08e9807229ee
Vladyslav Hibskyi <vgibskiy@gmail.com>
2025-05-02 07:18:14 +0300

    remove todo

--- src/main/java/ua/edu/ukma/hibskyi/messenger/MessengerApplication.java
+++ src/main/java/ua/edu/ukma/hibskyi/messenger/MessengerApplication.java
@@ -3,12 +3,6 @@ package ua.edu.ukma.hibskyi.messenger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// TODO:
-//  - add thymeleaf and views controllers
-//  - WebSocket for updating/deleting and for chats
-//  - remake mappers
-//  - remake services
-
 @SpringBootApplication
 public class MessengerApplication {
 
